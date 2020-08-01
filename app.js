484cdd81c0c8ece23f2f8f625799e307b8ef98cc
codeWriter9 <Sanjay.X.Ghosh@gmail.com>
2020-08-01 21:11:44 +0530

    Add - small improvements

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+const express = require('express');
+const path = require('path');
+const pug = require('pug');
+const app = express();
 
 // Define the port to run on
 app.set('port', 3000);
@@ -12,3 +13,4 @@ var server = app.listen(app.get('port'), function() {
   var port = server.address().port;
   console.log('Starting application on port ' + port);
 });
+
